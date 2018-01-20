ac5c30b833cadeed3e2cdb0e339e73cdd5561618
Jonathan <jonathanoudshoorn@gmail.com>
2018-01-20 10:23:25 +0100

    adding back collision for down movement (work in progress)

--- game/TetrisGameController.ts
+++ game/TetrisGameController.ts
@@ -3,15 +3,17 @@ import { TetrisActionName } from './game-objects/TetrisUtils';
 import { keyboardObservable$ } from './game-observers/KeyboardEventObserver';
 import { TetrisGraphics } from './TetrisGraphics';
 import { TetrisShape } from './game-objects/shape-objects/TetrisShape';
-import { getRandomTetrisShape$ } from './game-observers/RandomShapeGeneratorStreamObservable';
-import { Observable } from  'rxjs/Observable';
-import { combineLatest, tap} from 'rxjs/operators';
+import { getRandomTetrisShape$ } from './game-observers/RandomShapeGeneratorObservable';
+import { Observable } from 'rxjs/Observable';
+import { combineLatest, tap } from 'rxjs/operators';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/from';
+import { Subject } from 'rxjs/Subject';
 
 export class TetrisGameController {
 
     private tetrisGrid: TetrisGrid;
-    private movingShape$: Observable<TetrisShape>;
+    private movingShape$: Observable<TetrisShape> = new Observable();
     private score: number = 0;
     private numberOfRowsScored: number = 0;
 
@@ -20,28 +22,43 @@ export class TetrisGameController {
                 readonly tetrisGraphics: TetrisGraphics,
                 private tetrisScoreDiv: HTMLElement) {
         this.tetrisGrid = new TetrisGrid(numOfBlocksWide, numOfBlocksHigh);
-        this.movingShape$ = getRandomTetrisShape$();
         this.tetrisGraphics.drawBlocks(this.tetrisGrid.getAllBlocks());
         this.updateScore();
     }
 
     public startGame() {
+
+        var subject = new Subject();
+
+        subject.subscribe({
+            next: (v) => console.log('observerA: ' + v)
+        });
+        subject.subscribe({
+            next: (v) => console.log('observerB: ' + v)
+        });
+
+        var observable = Observable.from([1, 2, 3]);
+
+        observable.subscribe(subject);
+
+        this.movingShape$ = getRandomTetrisShape$();
+
         keyboardObservable$.pipe(
             combineLatest(this.movingShape$),
-            tap(([action, shape]) => this.performAction(shape, action))
+            tap(([ action, shape ]) => this.performAction(shape, action))
         ).subscribe(
-            ([, shape]) =>  {
+            ([ , shape ]) => {
                 this.tetrisGraphics.clearDraw();
                 this.tetrisGraphics.drawBlocks(this.tetrisGrid.getAllBlocks());
                 this.tetrisGraphics.drawBlocks(shape.blocks);
             }
         );
 
-        Observable.interval(200).pipe(
+        Observable.interval(300).pipe(
             combineLatest(this.movingShape$),
-            tap(([, shape]) => this.performAction(shape, TetrisActionName.DOWN))
+            tap(([ , shape ]) => this.performAction(shape, TetrisActionName.DOWN))
         ).subscribe(
-            ([, shape]) =>  {
+            ([ , shape ]) => {
                 this.tetrisGraphics.clearDraw();
                 this.tetrisGraphics.drawBlocks(this.tetrisGrid.getAllBlocks());
                 this.tetrisGraphics.drawBlocks(shape.blocks);
@@ -50,58 +67,38 @@ export class TetrisGameController {
 
     }
 
-    private collisionDetected(shape: TetrisShape, move: (shape:TetrisShape) => (void)): boolean {
+    private collisionDetected(shape: TetrisShape, move: (shape: TetrisShape) => (void)): boolean {
         let clonedShape: TetrisShape = shape.clone;
         clonedShape.performMove(move);
         return this.tetrisGrid.collisionDetection(clonedShape.blocks);
     }
 
-    // private moveDown(): Observable<boolean> {
-    //     let cloneShape: TetrisShape = this.movingShape.clone().moveDown();
-    //     if (this.tetrisGrid.blockCollisionDetection(cloneShape.blocks)) {
-    //         this.tetrisGrid.giveBlocksToGrid(this.movingShape.blocks);
-    //         let numOfFullRows: number[] = this.tetrisGrid.detectFullRows();
-    //         this.updateScore(numOfFullRows);
-    //         numOfFullRows.forEach((value) => this.tetrisGrid.removeRow(value));
-    //         this.movingShape = getRandomTetrisShape();
-    //     }
-    //     else if (this.tetrisGrid.collisionDetection(cloneShape.blocks)) {
-    //         this.movingShape.moveDown();
-    //     }
-    // }
-
-    // private rotate(): Observable<boolean> {
-    //     return this.movingShape$.pipe(
-    //         mergeMap(( shape ) => shape.clone$),
-    //         mergeMap(( clonedShape ) => clonedShape.rotate$),
-    //         mergeMap(( clonedShape ) => clonedShape.blocks$),
-    //         filter(( blocks ) => this.tetrisGrid.noCollisionDetected(blocks)),
-    //         combineLatest(this.movingShape$),
-    //         mergeMap(( [ blocks, shape ] ) => shape.rotate$),
-    //         map(() => true)
-    //     );
-    // }
-
     private performAction(shape: TetrisShape, action: TetrisActionName) {
 
         switch (action) {
             case TetrisActionName.LEFT:
-                if(!this.collisionDetected(shape, TetrisShape.moveLeft)){
+                if (!this.collisionDetected(shape, TetrisShape.moveLeft)) {
                     shape.performMove(TetrisShape.moveLeft);
                 }
                 break;
             case TetrisActionName.RIGHT:
-                if(!this.collisionDetected(shape, TetrisShape.moveRight)){
+                if (!this.collisionDetected(shape, TetrisShape.moveRight)) {
                     shape.performMove(TetrisShape.moveRight);
                 }
                 break;
             case TetrisActionName.DOWN:
-                if(!this.collisionDetected(shape, TetrisShape.moveDown)){
+                if (!this.collisionDetected(shape, TetrisShape.moveDown)) {
                     shape.performMove(TetrisShape.moveDown);
+                } else {
+                    this.tetrisGrid.giveBlocksToGrid(shape.blocks);
+                    let numOfFullRows: number[] = this.tetrisGrid.detectFullRows();
+                    this.updateScore(numOfFullRows);
+                    numOfFullRows.forEach((value) => this.tetrisGrid.removeRow(value));
+                    // TODO: generate new shape
                 }
                 break;
             case TetrisActionName.ROTATE:
-                if(!this.collisionDetected(shape, shape.rotate)){
+                if (!this.collisionDetected(shape, shape.rotate)) {
                     shape.performMove(shape.rotate);
                 }
         }
